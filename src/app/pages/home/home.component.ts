27758994a2724254255779bfc91d8eabb93ee5cd
Vinícius Henrique Dias de Souza <vinihds@gmail.com>
2025-08-07 15:51:07 -0300

    finalizando meu blog de filmes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  articles = dataFake;
+  mainArticle = this.articles[0]; // Primeiro artigo para o big-card
+  otherArticles = this.articles.slice(1, 4); // Próximos 3 artigos para small-cards
 
   constructor() { }
 
